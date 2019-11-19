099a74dacd4ab9c573aaa9914ea931b20bca4da4
Hmoretti <moretti.hernani@gmail.com>
2019-11-18 22:02:21 -0300

    implementando strutura django no home

--- website/views.py
+++ website/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    nome = 'Groger'
+    idade = 17
+    lista_roupas = [
+        'boné da lacoste',
+        'Boné Cyclone',
+        'cinto do batman',
+        'Toca da Medusa',
+        'Oculos rift zika'
+    ]
+
+    context = {
+        'roupas':lista_roupas,
+        'nome':nome,
+        'idade':idade
+    }
+    return render(request, 'home.html', context)
